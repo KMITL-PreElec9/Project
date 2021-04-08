c2592ea867979b09d4c98f32a8ced8621e073f30
Knapoj Chaimanekorn <63010104@kmitl.ac.th>
2021-04-08 19:53:27 +0700

    test again and again

--- preelec9/settings.py
+++ preelec9/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'preelec9-auth',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
