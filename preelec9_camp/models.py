031d6af67807a9608f822581ed46e17e78095ba3
Knapoj Chaimanekorn <63010104@kmitl.ac.th>
2021-04-12 21:49:21 +0700

    Model Bug Fixing

--- preelec9_camp/models.py
+++ preelec9_camp/models.py
@@ -3,4 +3,6 @@ from django.db import models
 # Create your models here.
 class Staff_auth(models.Model):
     email = models.CharField(max_length=200)
-    division = models.CharField(max_length=200)
+    division = models.CharField(max_length=200) 
+    def __str__(self):
+        return self.email
