e43507959fd70c2c614c88a8318f3b5be8ea14d0
Knapoj Chaimanekorn <63010104@kmitl.ac.th>
2021-04-22 17:42:23 +0700

    Test Django Form

--- dev_zone/views.py
+++ dev_zone/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from dev_zone.forms import ContactForm
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    submitted = False
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            return render(request, 'dev_zone/index.html', 
+                {
+                    'form': form, 
+                    'submitted': True,
+                    'name': cd['yourname'],
+                    'email': cd['email'],
+                    'subject': cd['subject'],
+                    'message': cd['message'],
+                    })
+    else:
+        form = ContactForm()
+        if 'submitted' in request.GET:
+            submitted = True 
+    return render(request, 'dev_zone/index.html', {'form': form, 'submitted': submitted})
